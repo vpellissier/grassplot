e49a58a811717e1baae6681a33ec13f3e9e67beb
vpellissier <vincent.pellissier@gmail.com>
2018-12-20 11:19:55 +0100

    Commit 11122018

--- listing_land_use.R
+++ listing_land_use.R
@@ -5,10 +5,11 @@ library(tidyverse)
 path_grassplot <- 'C:/Users/pellissi/Documents/GrassPlot composition data'
 path_git <- "C:/Users/pellissi/Documents/grassplot"
 # path_grassplot <- 'D:/Backup Aarhus 15122016/vincent/Grassplot/Preliminary work/GrassPlot composition data'
+# path_git <- 'C:/Users/vincent/Documents/grassplot'
 
 # dataframe with all the informations at a plot level
-df <- read_excel(file.path(path_grassplot, "GrassPlot 1.21.xlsx" ),
+df <- read_excel(file.path(path_grassplot, "GrassPlot 1.22.xlsx" ),
                                  sheet = 'Data', 
-                                 col_types = c(rep('guess', 94), rep('text', 10)))
+                                 col_types = c(rep('guess', 95), rep('text', 10)))
 
-saveRDS(df, file.path(path_git, 'Grassplot 1.21_Data.rds'))
+saveRDS(df, file.path(path_git, 'Grassplot 1.22_Data.rds'))
